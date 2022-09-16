d558442983e39fe1459a8b8ad393b95d46567878
karyseaweed <karyhdev@gmail.com>
2022-09-16 00:11:45 -0400

    TASK: add initial project files

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,30 @@
+import './FaceRecognition.css';
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img
+          id='inputImage'
+          src={imageUrl}
+          alt=''
+          width='500px'
+          height='auto'
+        />
+        {boxes.map((box) => (
+          <div
+            key={box.topRow}
+            className='bounding-box'
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
